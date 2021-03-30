c2a945fac965cd136a7fdce5eb62d4086fe43fda
SamNel2000 <alephnaught.notnull@gmail.com>
2021-03-29 23:37:20 -0500

    retrieved and piickled data

--- lichessAPI_test.py
+++ lichessAPI_test.py
@@ -1,9 +1,22 @@
-import berserk
+import berserk, pickle
 from the_token import access_token
 
 session = berserk.TokenSession(access_token)
 client = berserk.Client(session=session)
 
 #print(client.users.get_public_data('AlephNaught202'))
-games = client.games.export_by_player('AlephNaught202', max=10, as_pgn = True)
-print(list(games)[0])
+games = list(client.games.export_by_player('AlephNaught202', max=10, as_pgn = True))
+#print(games[0])
+
+GamesPlayer = list(client.games.export_by_player('AlephNaught202', max=100, as_pgn = True))
+with open('Last-100-games-AlephNaught202.pkl', 'wb') as f:
+    pickle.dump(GamesPlayer,f)
+with open('Last-100-games-AlephNaught202.pkl', 'rb') as f:
+    GamesPlayer = pickle.load(f)    
+
+for x in GamesPlayer:
+	print("GAME:")
+	print(x)
+
+	#retrieves last 100 games of a given player name.
+	#with this I can retrieve any game data I need.
